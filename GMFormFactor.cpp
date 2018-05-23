f94a76909e15a8ccef37ce59b287505c1c41bc4a
Yongcheng Wu <yc_wu1030@126.com>
2018-05-23 11:19:35 -0400

    Modify the main file for data generation

--- GMFormFactor.cpp
+++ GMFormFactor.cpp
@@ -10,9 +10,10 @@ using namespace GM;
 using namespace std;
 
 int main() {
-    std::ofstream BRS("GMBRScalars.dat");
-    // std::ofstream BRH30("GMBRH30.dat");
-    // std::ofstream BRH50("GMBRH50.dat");
+    // std::ofstream BRS("GMBRScalars.dat");
+    std::ofstream BRH("GMBRH.dat");
+    std::ofstream BRH3("GMBRH3.dat");
+    std::ofstream BRH5("GMBRH5.dat");
     // std::ofstream BRH3p("GMBRH3p.dat");
     // std::ofstream BRH5p("GMBRH5p.dat");
     // std::ofstream BRH5pp("GMBRH5pp.dat");
@@ -29,12 +30,14 @@ int main() {
     GMModel Mod;
     clock_t T1, T2, T3;
     GMDecay decay;
-    BRS<<"# mphi dM BRHAA BRHZA BRHZZ BRHWW GammaH BRHAAEFT BRHZAEFT BRHZZEFT BRHWWEFT GammaHEFT";
-    BRS<<" BRH30AA BRH30ZA BRH30ZZ BRH30WW GammaH30 BRH30AAEFT BRH30ZAEFT BRH30ZZEFT BRH30WWEFT GammaH30EFT";
-    BRS<<" BRH50AA BRH50ZA BRH50ZZ BRH50WW GammaH50 BRH50AAEFT BRH50ZAEFT BRH50ZZEFT BRH50WWEFT GammaH50EFT";
-    BRS<<" BRH3pWA BRH3pWZ GammaH3p BRH3pWAEFT BRH3pWZEFT GammaH3pEFT";
-    BRS<<" BRH5pWA BRH5pWZ GammaH5p BRH5pWAEFT BRH5pWZEFT GammaH5pEFT";
-    BRS<<" BRH5ppWW GammaH5pp BRH5ppWWEFT GammaH5ppEFT"<<endl;
+    // BRS<<"# mphi dM BRHAA BRHZA BRHZZ BRHWW GammaH BRHAAEFT BRHZAEFT BRHZZEFT BRHWWEFT GammaHEFT";
+    // BRS<<" BRH30AA BRH30ZA BRH30ZZ BRH30WW GammaH30 BRH30AAEFT BRH30ZAEFT BRH30ZZEFT BRH30WWEFT GammaH30EFT";
+    // BRS<<" BRH50AA BRH50ZA BRH50ZZ BRH50WW GammaH50 BRH50AAEFT BRH50ZAEFT BRH50ZZEFT BRH50WWEFT GammaH50EFT";
+    // BRS<<" BRH3pWA BRH3pWZ GammaH3p BRH3pWAEFT BRH3pWZEFT GammaH3pEFT";
+    // BRS<<" BRH5pWA BRH5pWZ GammaH5p BRH5pWAEFT BRH5pWZEFT GammaH5pEFT";
+    // BRS<<" BRH5ppWW GammaH5pp BRH5ppWWEFT GammaH5ppEFT"<<endl;
+// For H5:
+    BRH5<<"# mphi dM BRH50AA BRH50ZA BRH50ZZ BRH50WW GammaH50 BRH50AAEFT BRH50ZAEFT BRH50ZZEFT BRH50WWEFT GammaH50EFT BRH5pWA BRH5pWZ GammaH5p BRH5pWAEFT BRH5pWZEFT GammaH5pEFT BRH5ppWW GammaH5pp BRH5ppWWEFT GammaH5ppEFT"<<endl;
     for (mphi = 80.0; mphi<=500 ; mphi+=5)
     {
         for (dM = 0; dM < 200; dM+=10)
@@ -44,68 +47,94 @@ int main() {
             Mod.MH5 = mphi;
             Mod.M2 = 80.0;
             decay.SetModel(Mod);
-            GHAA = decay.GetGammaHVV(H,A,A);
-            GHZA = decay.GetGammaHVV(H,Z,A);
-            GHZZ = decay.GetGammaHVV(H,Z,Z);
-            GHWW = decay.GetGammaHVV(H,W,W);
-            GHtot = decay.GetGammaHtot(H);
-            BRS<<mphi<<"  "<<dM<<"  "<<GHAA/GHtot<<"  "<<GHZA/GHtot<<"  "<<GHZZ/GHtot<<"  "<<GHWW/GHtot<<"  "<<GHtot;
-            GHAA = decay.GetGammaHVVEFT(H,A,A);
-            GHZA = decay.GetGammaHVVEFT(H,Z,A);
-            GHZZ = decay.GetGammaHVVEFT(H,Z,Z);
-            GHWW = decay.GetGammaHVVEFT(H,W,W);
-            GHtot = decay.GetGammaHtotEFT(H);
-            BRS<<"  "<<GHAA/GHtot<<"  "<<GHZA/GHtot<<"  "<<GHZZ/GHtot<<"  "<<GHWW/GHtot<<"  "<<GHtot;
-
-            GH30AA = decay.GetGammaHVV(H30,A,A);
-            GH30ZA = decay.GetGammaHVV(H30,Z,A);
-            GH30ZZ = decay.GetGammaHVV(H30,Z,Z);
-            GH30WW = decay.GetGammaHVV(H30,W,W);
-            GH30tot = decay.GetGammaHtot(H30);
-            BRS<<"  "<<GH30AA/GH30tot<<"  "<<GH30ZA/GH30tot<<"  "<<GH30ZZ/GH30tot<<"  "<<GH30WW/GH30tot<<"  "<<GH30tot;
-            GH30AA = decay.GetGammaHVVEFT(H30,A,A);
-            GH30ZA = decay.GetGammaHVVEFT(H30,Z,A);
-            GH30ZZ = decay.GetGammaHVVEFT(H30,Z,Z);
-            GH30WW = decay.GetGammaHVVEFT(H30,W,W);
-            GH30tot = decay.GetGammaHtotEFT(H30);
-            BRS<<"  "<<GH30AA/GH30tot<<"  "<<GH30ZA/GH30tot<<"  "<<GH30ZZ/GH30tot<<"  "<<GH30WW/GH30tot<<"  "<<GH30tot;
 
             GH50AA = decay.GetGammaHVV(H50,A,A);
             GH50ZA = decay.GetGammaHVV(H50,Z,A);
             GH50ZZ = decay.GetGammaHVV(H50,Z,Z);
             GH50WW = decay.GetGammaHVV(H50,W,W);
             GH50tot = decay.GetGammaHtot(H50);
-            BRS<<"  "<<GH50AA/GH50tot<<"  "<<GH50ZA/GH50tot<<"  "<<GH50ZZ/GH50tot<<"  "<<GH50WW/GH50tot<<"  "<<GH50tot;
+            BRH5<<mphi<<"  "<<dM<<"  "<<GH50AA/GH50tot<<"  "<<GH50ZA/GH50tot<<"  "<<GH50ZZ/GH50tot<<"  "<<GH50WW/GH50tot<<"  "<<GH50tot;
             GH50AA = decay.GetGammaHVVEFT(H50,A,A);
             GH50ZA = decay.GetGammaHVVEFT(H50,Z,A);
             GH50ZZ = decay.GetGammaHVVEFT(H50,Z,Z);
             GH50WW = decay.GetGammaHVVEFT(H50,W,W);
             GH50tot = decay.GetGammaHtotEFT(H50);
-            BRS<<"  "<<GH50AA/GH50tot<<"  "<<GH50ZA/GH50tot<<"  "<<GH50ZZ/GH50tot<<"  "<<GH50WW/GH50tot<<"  "<<GH50tot;
-
-            GH3pWA = decay.GetGammaHVV(H3p,W,A);
-            GH3pWZ = decay.GetGammaHVV(H3p,W,Z);
-            GH3ptot = decay.GetGammaHtot(H3p);
-            BRS<<"  "<<GH3pWA/GH3ptot<<"  "<<GH3pWZ/GH3ptot<<"  "<<GH3ptot;
-            GH3pWA = decay.GetGammaHVVEFT(H3p,W,A);
-            GH3pWZ = decay.GetGammaHVVEFT(H3p,W,Z);
-            GH3ptot = decay.GetGammaHtotEFT(H3p);
-            BRS<<"  "<<GH3pWA/GH3ptot<<"  "<<GH3pWZ/GH3ptot<<"  "<<GH3ptot;
+            BRH5<<"  "<<GH50AA/GH50tot<<"  "<<GH50ZA/GH50tot<<"  "<<GH50ZZ/GH50tot<<"  "<<GH50WW/GH50tot<<"  "<<GH50tot;
 
             GH5pWA = decay.GetGammaHVV(H5p,W,A);
             GH5pWZ = decay.GetGammaHVV(H5p,W,Z);
             GH5ptot = decay.GetGammaHtot(H5p);
-            BRS<<"  "<<GH5pWA/GH5ptot<<"  "<<GH5pWZ/GH5ptot<<"  "<<GH5ptot;
+            BRH5<<"  "<<GH5pWA/GH5ptot<<"  "<<GH5pWZ/GH5ptot<<"  "<<GH5ptot;
             GH5pWA = decay.GetGammaHVVEFT(H5p,W,A);
             GH5pWZ = decay.GetGammaHVVEFT(H5p,W,Z);
             GH5ptot = decay.GetGammaHtotEFT(H5p);
-            BRS<<"  "<<GH5pWA/GH5ptot<<"  "<<GH5pWZ/GH5ptot<<"  "<<GH5ptot;
+            BRH5<<"  "<<GH5pWA/GH5ptot<<"  "<<GH5pWZ/GH5ptot<<"  "<<GH5ptot;
 
             GH5ppWW = decay.GetGammaHVV(H5pp,W,W);
-            BRS<<"  "<<1.0<<"  "<<GH5ppWW;
+            BRH5<<"  "<<1.0<<"  "<<GH5ppWW;
             GH5ppWW = decay.GetGammaHVVEFT(H5pp,W,W);
-            BRS<<"  "<<1.0<<"  "<<GH5ppWW<<endl;
+            BRH5<<"  "<<1.0<<"  "<<GH5ppWW<<endl;
 
+        }
+    }
+// For H:
+    BRH<<"# mphi dM BRHAA BRHZA BRHZZ BRHWW GammaH BRHAAEFT BRHZAEFT BRHZZEFT BRHWWEFT GammaHEFT"<<endl;
+    for (mphi = 80.0; mphi<=500 ; mphi+=5)
+    {
+        for (dM = 0; dM < 200; dM+=10)
+        {
+            Mod.MH = mphi;
+            Mod.MH3 = sqrt(mphi*mphi-1.0/2.0*dM*dM);
+            Mod.MH5 = sqrt(mphi*mphi-3.0/2.0*dM*dM);
+            Mod.M2 = 80.0;
+            decay.SetModel(Mod);
+            GHAA = decay.GetGammaHVV(H,A,A);
+            GHZA = decay.GetGammaHVV(H,Z,A);
+            GHZZ = decay.GetGammaHVV(H,Z,Z);
+            GHWW = decay.GetGammaHVV(H,W,W);
+            GHtot = decay.GetGammaHtot(H);
+            BRH<<mphi<<"  "<<dM<<"  "<<GHAA/GHtot<<"  "<<GHZA/GHtot<<"  "<<GHZZ/GHtot<<"  "<<GHWW/GHtot<<"  "<<GHtot;
+            GHAA = decay.GetGammaHVVEFT(H,A,A);
+            GHZA = decay.GetGammaHVVEFT(H,Z,A);
+            GHZZ = decay.GetGammaHVVEFT(H,Z,Z);
+            GHWW = decay.GetGammaHVVEFT(H,W,W);
+            GHtot = decay.GetGammaHtotEFT(H);
+            BRH<<"  "<<GHAA/GHtot<<"  "<<GHZA/GHtot<<"  "<<GHZZ/GHtot<<"  "<<GHWW/GHtot<<"  "<<GHtot<<endl;
+
+        }
+    }
+// For H3:
+    BRH3<<"# mphi dM BRH30AA BRH30ZA BRH30ZZ BRH30WW GammaH30 BRH30AAEFT BRH30ZAEFT BRH30ZZEFT BRH30WWEFT GammaH30EFT BRH3pWA BRH3pWZ GammaH3p BRH3pWAEFT BRH3pWZEFT GammaH3pEFT"<<endl;
+    for (mphi = 80.0; mphi<=500 ; mphi+=5)
+    {
+        for (dM = 0; dM < 200; dM+=10)
+        {
+            Mod.MH3 = mphi;
+            Mod.MH = sqrt(mphi*mphi+1.0/2.0*dM*dM);
+            Mod.MH5 = sqrt(mphi*mphi-dM*dM);
+            Mod.M2 = 80.0;
+            decay.SetModel(Mod);
+            GH30AA = decay.GetGammaHVV(H30,A,A);
+            GH30ZA = decay.GetGammaHVV(H30,Z,A);
+            GH30ZZ = decay.GetGammaHVV(H30,Z,Z);
+            GH30WW = decay.GetGammaHVV(H30,W,W);
+            GH30tot = decay.GetGammaHtot(H30);
+            BRH3<<mphi<<"  "<<dM<<"  "<<GH30AA/GH30tot<<"  "<<GH30ZA/GH30tot<<"  "<<GH30ZZ/GH30tot<<"  "<<GH30WW/GH30tot<<"  "<<GH30tot;
+            GH30AA = decay.GetGammaHVVEFT(H30,A,A);
+            GH30ZA = decay.GetGammaHVVEFT(H30,Z,A);
+            GH30ZZ = decay.GetGammaHVVEFT(H30,Z,Z);
+            GH30WW = decay.GetGammaHVVEFT(H30,W,W);
+            GH30tot = decay.GetGammaHtotEFT(H30);
+            BRH3<<"  "<<GH30AA/GH30tot<<"  "<<GH30ZA/GH30tot<<"  "<<GH30ZZ/GH30tot<<"  "<<GH30WW/GH30tot<<"  "<<GH30tot;
+
+            GH3pWA = decay.GetGammaHVV(H3p,W,A);
+            GH3pWZ = decay.GetGammaHVV(H3p,W,Z);
+            GH3ptot = decay.GetGammaHtot(H3p);
+            BRH3<<"  "<<GH3pWA/GH3ptot<<"  "<<GH3pWZ/GH3ptot<<"  "<<GH3ptot;
+            GH3pWA = decay.GetGammaHVVEFT(H3p,W,A);
+            GH3pWZ = decay.GetGammaHVVEFT(H3p,W,Z);
+            GH3ptot = decay.GetGammaHtotEFT(H3p);
+            BRH3<<"  "<<GH3pWA/GH3ptot<<"  "<<GH3pWZ/GH3ptot<<"  "<<GH3ptot<<endl;
         }
     }
         // T1 = clock();
